a7f93e19f683215428c3696f58bbae894b401f4b
zhenglongzhen <zhenglongzhen0518@126.com>
2017-03-18 00:55:08 +0800

    提交

---
+++ src/main/java/velocity/example1/Person.java
@@ -0,0 +1,19 @@
+package velocity.example1;
+
+/**
+ * Created by zhenglongzhen on 2015/8/18.
+ */
+public class Person {
+    public static final String ALAIS_NAME = "姓名";
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
